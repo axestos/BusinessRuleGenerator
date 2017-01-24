275b8ad3e94866811d71880780c5eef09b20a080
Quinten Peels <quinten@linux.com>
2017-01-24 15:00:54 +0100

    Restful

---
+++ src/restful/TestConnection.java
@@ -0,0 +1,52 @@
+package restful;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by bluedog on 1/24/2017.
+ */
+public class TestConnection {
+
+    // http://localhost:8080/RESTfulExample/json/product/get
+    public static void main(String[] args) {
+
+        try {
+
+            URL url = new URL("https://ondora02.hu.nl:8080/ords/stud1681260/123getBR");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+}
