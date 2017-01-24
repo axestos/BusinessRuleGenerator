c3aeaf04cef2085331780dbfb8c75267d52193d1
akatar1 <kevin.w@live.nl>
2017-01-24 14:52:17 +0100

    Attribute Range Rule

--- src/businessrule/AttributeRangeRule.java
+++ src/businessrule/AttributeRangeRule.java
@@ -2,10 +2,31 @@ package businessrule;
 
 public class AttributeRangeRule extends BusinessRule {
 
-	public AttributeRangeRule(int ruleid, String authorid, String type, String operator, String first, String last) {
+	public AttributeRangeRule(int ruleid, String authorid, String type, String operator, String compare,String rangeAttribute, String first, String last) {
 		super(ruleid, authorid, type, operator, compare, first, last);
-		// TODO Auto-generated constructor stub
+		generateAttributeRangeRule(ruleid, authorid, type, operator, rangeAttribute, first, last);
 	}
 
-
+	private void generateAttributeRangeRule(int ruleid, String authorid, String type, String operator, String rangeAttribute, String first, String last){
+		String [] tableAttribute = rangeAttribute.split("\\.");
+		String tablename = tableAttribute[0];
+		String attribute = tableAttribute[1];
+		String constrainname = "constraint"+ruleid;
+		String constraintstatement = "("+attribute+" "+getOperator(operator)+" "+first+" and "+last+");";
+		System.out.println(toString(tablename, constrainname, constraintstatement));
+	}
+	public String toString(String tablename, String constrainname, String constraintstatement){
+		String string = "Alter table " + tablename +" add constraint " + constrainname + " check"+constraintstatement;
+		return string;
+	}
+	
+	public String getOperator(String operator){
+		if (operator.equals("NotBetween")){
+			operator = "not between";
+		}
+		else if (operator.equals("Between")){
+			operator = "between";
+		}
+		return operator;
+	}
 }
--- src/businessrule/Main.java
+++ src/businessrule/Main.java
@@ -3,7 +3,7 @@ package businessrule;
 public class Main {
 
 	public static void main(String[] args) {
-
+		new AttributeRangeRule(100, "Kevin", "Attribute", "NotBetween", "", "School.klaslokaal", "10", "150");
 	}
 
 }
