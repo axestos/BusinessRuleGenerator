7e657801a589a4164698992edc966898f7fcd17c
akatar1 <kevin.w@live.nl>
2017-01-24 11:43:40 +0100

    .

--- src/dao/BaseDAO.java
+++ src/dao/BaseDAO.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class BaseDAO {
 	public static final String ORACLE_JDBC_CONN = "jdbc:oracle:thin:@//ondora02.hu.nl:8521/cursus02.hu.nl";
-	public static final String ORACLE_JDBC_USER = "stud1681260";// Database name zonder CAPS
-	public static final String ORACLE_JDBC_PASSWORD = "stud1681260";// Database name zonder CAPS
+	public static final String ORACLE_JDBC_USER = "stud1681260";
+	public static final String ORACLE_JDBC_PASSWORD = "stud1681260";
 
 	protected final Connection getConnection() {
 		try {
@@ -14,10 +14,8 @@ public class BaseDAO {
 				Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
 				System.out.println("-----Driver found-----");
 			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} catch (ClassNotFoundException e) {
@@ -26,9 +24,11 @@ public class BaseDAO {
 		}
 		Connection con = null;
 		try {
-			System.out.println("Trying to establish connection");
+			System.out.println("-----Trying to establish connection-----");
 			con = DriverManager.getConnection(ORACLE_JDBC_CONN, ORACLE_JDBC_USER, ORACLE_JDBC_PASSWORD);
+			System.out.println("-----Connected to the database-----");
 		} catch (Exception ex) {
+			System.out.println("-----Could not establish connection to database, read the errorlog-----");
 			throw new RuntimeException(ex);
 		}
 		return con;
