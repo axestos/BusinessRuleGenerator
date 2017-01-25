1ec7e926bc40b21954ad912d8b048115fea1f678
akatar1 <kevin.w@live.nl>
2017-01-25 15:39:15 +0100

    Inter Entity Compare rule wordt nu goed aangemaakt

--- src/businessrule/Main.java
+++ src/businessrule/Main.java
@@ -8,6 +8,6 @@ public class Main {
 //		new AttributeListRule(102, "Quinten",  "Attribute", "In", "Order.status", "'shipped','in warehouse','deliverd','waiting on invoice'");
 //		new AttributeOtherRule(103, "Berend", "Attribute", "Between", "Voetbal.veld", "2", "4", "1,1");
 //		new TupleCompareRule(104, "Roelant", "Tuple", "GreaterThan", "Voetbal.team", "Voetbal.elftal");
-		new InterEntityCompareRule(105,"Dennis", "InterEntity", "GreaterThan", "School.klaslokaal", "Klas.leerlingen", true, "Mag niet");
+		new InterEntityCompareRule(105,"Dennis", "InterEntity", "GreaterThan", "School.klaslokaal", "Klas.leerlingen", true, "Leerlingen in een Klas mag niet groter zijn dan Klaslokaal in School");
 	}
 }
