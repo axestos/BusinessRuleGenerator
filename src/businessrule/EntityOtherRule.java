ec3a0514196cd1a451b18ad520cfcddf26679b56
Victor <me@victorvos.com>
2017-02-01 14:56:48 +0100

    Entity Other Rule changes

--- src/businessrule/EntityOtherRule.java
+++ src/businessrule/EntityOtherRule.java
@@ -6,18 +6,17 @@ public class EntityOtherRule extends BusinessRule {
 	public EntityOtherRule(int ruleid, String authorid, String type, String operator, String first, String last, String errorCode, String firstValue, String beforeAfter) {
 		super(ruleid, authorid, type, operator, first, last);
 		setFirstValue(firstValue);
-		generateOtherRule(ruleid, first, last, errorCode, beforeAfter);
+		generateOtherRule(ruleid, first, errorCode, beforeAfter);
 	}
 
-	private void generateOtherRule(int ruleid, String first, String last, String errorCode, String beforeAfter){
+	private void generateOtherRule(int ruleid, String first, String errorCode, String beforeAfter){
 		String attrTable1 = first.split("\\.")[1];
 		String tablename_attr1 = first.split("\\.")[0];
-		String tablename_attr2 = last.split("\\.")[0];
 		String triggernameTable1 = tablename_attr1+type+ruleid;
-		setGeneratedCode(toString(triggernameTable1, attrTable1, tablename_attr2, tablename_attr1, errorCode, beforeAfter));
+		setGeneratedCode(toString(triggernameTable1, attrTable1, tablename_attr1, errorCode, beforeAfter));
 	}
 
-	private String toString(String triggername, String attrTable1, String tablename_attr2, String tablename_attr1, String errorCode, String beforeAfter) {
+	private String toString(String triggername, String attrTable1, String tablename_attr1, String errorCode, String beforeAfter) {
 		String generatedDeclare = "Create or replace trigger "+triggername+
 				"\n"+beforeAfter+" insert or update on "+tablename_attr1+
 				"\nfor each row"+
@@ -25,9 +24,9 @@ public class EntityOtherRule extends BusinessRule {
 				"l_passed boolean := true;\n"+
 				""+tablename_attr1+"\n";
 		String generateBegin = "BEGIN\n"+
-				"SELECT "+tablename_attr1+"."+attrTable1+"\n"+
+				"SELECT count(*) into l_aantal\n"+
 				"from "+tablename_attr1+
-				"\nwhere "+tablename_attr1+"_id = p_"+tablename_attr1+"_row.new_"+tablename_attr2+"_id;\n"+
+				"\nwhere "+tablename_attr1+"_"+attrTable1+" = p_"+tablename_attr1+"_row.new_"+tablename_attr1+"_id;\n"+
 				"l_passed := "+attrTable1+" "+getOperator(operator)+" "+firstValue+"\n"+
 				"if not l_passed then\n"+
 				"raise_application_error (-20800,'"+errorCode+"');\n"+
@@ -37,8 +36,6 @@ public class EntityOtherRule extends BusinessRule {
 		return generatedDeclare + generateBegin;
 	}
 
-	
-	
 	
 	public String getOperator(String operator){
 		if (operator.equals("NotEquals")){
